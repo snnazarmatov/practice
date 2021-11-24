4a07167c217c550ce67e61f46b0d8baadaf27580
Esen Nazarmatov <snnazarmatov@gmail.com>
2021-11-24 19:08:05 +0600

    pratice

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React,{useState} from 'react';
+import AddUser from './Components/Users/AddUser'
+import UsersList from './Components/Users/UsersList';
+
+
+function App() {
+  const [userList, setUserList] = useState([]);
+  const addUserHandler = (uName, uAge) => {
+    setUserList((prevUserList) => {
+      return [
+        ...prevUserList,
+        {name: uName, age: uAge, id: Math.random().toString()},
+      ]
+    })
+  };
+
+  return (
+    <>
+    <AddUser onAddUser={addUserHandler}/>
+    <UsersList users={userList}/>
+      
+    </>
+  )
+}
+
+export default App;
+

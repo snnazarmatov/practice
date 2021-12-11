99dfc8ef952bfe15e8654f4e3f08819fb4b165a7
Esen Nazarmatov <snnazarmatov@gmail.com>
2021-12-11 08:48:34 +0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React,{useState,useEffect,useCallback} from 'react';
 import MoviesLists from './Components/MoviesLists';
 import './App.css'
+import AddMovie from './Components/AddMovie';
 
 
 const App = () => {
@@ -23,7 +24,7 @@ const App = () => {
     setIsLoading(true)
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films')
+      const response = await fetch('https://react-http-test-c76c4-default-rtdb.firebaseio.com/movies.json')
 
       //1 backend тен келген ката учун
       if(!response.ok) {
@@ -32,17 +33,28 @@ const App = () => {
       }
       //response пустой болушу мумкун сунда аны кутуптур деп await jazabyz
       const data = await response.json();
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-          director: movieData.director,
-          producer: movieData.producer
-        }
-      })
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+
+      for(const key in data){
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+        })
+      }
+
+      // const transformedMovies = data.results.map(movieData => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //     director: movieData.director,
+      //     producer: movieData.producer
+      //   }
+      // })
+      setMovies(loadedMovies);
       //fetch ten ketktn kata uchun
     }catch (error){
       setError(error.message)
@@ -57,6 +69,25 @@ const App = () => {
     fetchMoviesHandler();
 
   }, [fetchMoviesHandler]);
+
+
+
+
+  // post 
+  async function addMovieHandler(movie){
+    const response = await fetch('https://react-http-test-c76c4-default-rtdb.firebaseio.com/movies.json',{
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers:{
+        'Content-type': 'application/json'
+      }
+    })
+    const data = await response.json();
+    console.log(data);
+  }
+
+
+
   // **************
 //movies тер табылбай калган учурда 
   let content = <p>Found no movies...</p>
@@ -74,8 +105,9 @@ const App = () => {
   return (
     <React.Fragment>
       <section>
+        <AddMovie onAddMovie={addMovieHandler}/>
         {/* <button onClick={fetchMoviesHandler}>Fetch movies</button> */}
-        <h1>Movies:</h1>
+        {/* <h1>Movies:</h1> */}
         {/* <p>{time}</p> */}
       </section>
       <section>
